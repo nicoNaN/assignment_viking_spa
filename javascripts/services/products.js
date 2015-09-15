bd4accdd8d70795c649503df36f0f7dbfc1b1660
niconan <nick.hax@gmail.com>
2015-09-14 22:21:45 -0700

    categories to products

--- javascripts/services/products.js
+++ javascripts/services/products.js
@@ -1,12 +1,22 @@
 storefront.factory('products', function() {
   var products = [];
+  var categories = [];
+
+  for (i = 1; i <= 10; i++) {
+    var newCategory = {
+      name: faker.commerce.department()
+    };
+
+    categories.push(newCategory);
+  }
 
   for (i = 1; i <= 50; i++) {
     var newProd = {
       id: i,
       price: faker.commerce.price(),
       name: faker.commerce.productName(),
-      description: faker.lorem.paragraph()
+      description: faker.lorem.paragraph(),
+      category: categories[Math.floor(Math.random()*categories.length)]
     };
 
     products.push(newProd);
